706e50c1c66f9fae6993c55e5945dd5e5c3bc5b1
Erik Ly <erikly11@gmail.com>
2023-10-12 13:07:01 -0500

    Included tables for medication and accounts

---
+++ src/main/java/com/RWWR/prescriptionmanagementsystem/Model/Account.java
@@ -0,0 +1,25 @@
+package com.RWWR.prescriptionmanagementsystem.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Account {
+    @Id
+    @SequenceGenerator(
+        name = "account_id_sequence", 
+        sequenceName = "account_id_sequence",
+        // this sets how much the id increases in the sequence. 
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE, 
+        generator = "account_sequence"
+    )
+    private int id;
+
+    
+}
