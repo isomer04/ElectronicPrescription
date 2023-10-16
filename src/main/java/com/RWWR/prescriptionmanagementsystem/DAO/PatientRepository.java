bc3b59738680de10cf60191e2ad019eadedf7f5e
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-16 13:33:24 -0400

    Moved backend files to the API folder

--- src/main/java/com/RWWR/prescriptionmanagementsystem/DAO/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.RWWR.prescriptionmanagementsystem.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.RWWR.prescriptionmanagementsystem.Model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient,String>{
-
-}
